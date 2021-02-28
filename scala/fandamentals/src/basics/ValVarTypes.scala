86a8e503c7f1a4ac237893cee792ac0888c97ad5
Ali Mohammadpour <aliemohammadpour@gmail.com>
2021-02-28 13:56:23 +0330

    feat: ValVarTypes

--- scala/fandamentals/src/basics/ValVarTypes.scala
+++ scala/fandamentals/src/basics/ValVarTypes.scala
@@ -1,5 +1,21 @@
 package basics
 
 object ValVarTypes extends App {
-  println("Hello, ValVarTypes")
+
+  // Value Types are immutable
+  val anInt: Int = 4
+  val aBoolean: Boolean = false
+  val aString: String = "Hello, World!"
+  val aChar: Char = 'a'
+  val aShort: Short = 1234
+  val aLong: Long = 1234567890L
+  val aFloat: Float = 2.0f
+  val aDoable: Double = 3.14
+
+
+  // variables are mutable
+  var aVariable: Int = 4
+
+  println(aVariable + anInt + aLong)
+
 }
